193045856a9a5c3c511adfa82b23c1c4e2a308c9
chihyen <chihyencheng@wondercore.com>
2021-12-29 11:11:06 +0800

    init

---
+++ 202112229_TestGit/202112229_TestGit/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  202112229_TestGit
+//
+//  Created by wondercise on 2021/12/29.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
