81e4b7dbb4737996fc0a41cb946ef7f0c1b2d7bf
chihyen <chihyencheng@wondercore.com>
2021-12-30 09:24:02 +0800

    add 3-2

--- 202112229_TestGit/202112229_TestGit/ViewController.swift
+++ 202112229_TestGit/202112229_TestGit/ViewController.swift
@@ -12,7 +12,7 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         print("aaaa")
-        print("b")
+        print("bb")
         // Do any additional setup after loading the view.
     }
 
